a7132098b4be837f35c1d2cf60f62c13d5a17b87
yashjain1244 <yashsarsamkar@gmail.com>
2025-09-04 23:21:00 +0530

    change

--- src/com/lexus/bank/application/utilityclasses/SavingAccountUtility.java
+++ src/com/lexus/bank/application/utilityclasses/SavingAccountUtility.java
@@ -18,14 +18,14 @@ public class SavingAccountUtility {
                 case 1 -> {
                     System.out.println("How much Money your want to Deposit :- ");
                     savingAccount.depositMoney(scanner.nextInt());
-                    SavingAccountUtility.SavingAccountBalance(savingAccount);
+                    SavingAccountUtility.savingAccountBalance(savingAccount);
                 }
                 case 2 -> {
                     System.out.println("How much Money you want to Withdraw :- ");
                     savingAccount.withdrawMoney(scanner.nextInt());
-                    SavingAccountUtility.SavingAccountBalance(savingAccount);
+                    SavingAccountUtility.savingAccountBalance(savingAccount);
                 }
-                case 3 -> SavingAccountUtility.SavingAccountBalance(savingAccount);
+                case 3 -> SavingAccountUtility.savingAccountBalance(savingAccount);
                 case 0 -> System.out.println("Transaction Cancelled...");
                 default -> System.out.println("Invalid Input. Please Try Again.");
             }
@@ -41,7 +41,7 @@ public class SavingAccountUtility {
         return scanner.nextInt();
     }
 
-    private static void SavingAccountBalance(SavingAccount savingAccount){
+    private static void savingAccountBalance(SavingAccount savingAccount){
         System.out.println("Your Available Account Balance :- " +
                 savingAccount.getAccountBalance()+"/- Rs.");
         System.out.println();
